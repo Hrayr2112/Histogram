0c615485ece602279c9dbfd1181a2fae17f3e967
Hrayr2112 <hrayr.yeghiazaryan@gmail.com>
2019-12-26 01:21:43 +0400

    Initial Commit

---
+++ Histogram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Histogram
+//
+//  Created by Hrayr Yeghiazaryan on 26.12.2019.
+//  Copyright © 2019 Hrayr Yeghiazaryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
